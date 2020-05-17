af39e9da0c357aaea1f08a4507e2919a977c229e
BoldErdene1221 <azu.wert@gmail.com>
2020-05-17 23:06:31 +0700

    sound added

--- js/bday.js
+++ js/bday.js
@@ -2,6 +2,8 @@ function alertOnbody() {
     alert(
         "Хүслээ шивнээд бялуун дээр дарж ЛАА үлээгээрэй! 10 секундын дараа бэлэг гарч ирнэ шүү хэхэ :) сайхан баярлаарай миний гүнж хайртай шүү <3"
     );
+    var audio = new Audio('../music/birthday.mp3');
+    audio.play();
 }
 
 function onClickCake() {
