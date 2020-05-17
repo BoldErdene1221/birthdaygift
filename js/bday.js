c641ffa1a6791d79516825bda55545c04ef67cb1
BoldErdene1221 <azu.wert@gmail.com>
2020-05-17 16:33:50 +0700

    gift

---
+++ js/bday.js
@@ -0,0 +1,13 @@
+function alertOnbody() {
+    alert(
+        "Хүслээ шивнээд бялуу дээрээ дарж ЛАА үлээгээрэй! 10 секундын дараа бэлэг гарч ирнэ шүү хэхэ :) Миний гүнж хайртай шүү <3"
+    );
+}
+
+function onClickCake() {
+    document.getElementById("flame").style.display = "none";
+    // window.location.href = "/cv.html", "_blank";
+    setTimeout(function () {
+        window.open("/cv.html", "_blank")
+    }, 10000);
+}
